543065687ddef765228a22b81c2b62c8c2565c28
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2025-11-11 23:38:52 +0200

    Repository is done. Services next.

--- src/main/java/com/claude/lesson4/repositories/TaskRepository.java
+++ src/main/java/com/claude/lesson4/repositories/TaskRepository.java
@@ -1,15 +1,43 @@
 package com.claude.lesson4.repositories;
 
 import com.claude.lesson4.models.Task;
+import com.claude.lesson4.models.TaskStatus;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class TaskRepository {
     private Map<Long, Task> tasks = new HashMap<>();
     Long id = 0L;
+
+    public TaskRepository() {
+        System.out.println("[IoC] Bean TaskRepository created");
+        this.initializeTasks();
+    }
+
+    @PostConstruct
+    void postConstruct() {
+        System.out.println("[TaskRepository] Initialized tasks: " + tasks.size());
+    }
+
+    private void initializeTasks() {
+        Map<Long, Task> newTasks = new HashMap<>();
+        Task task1 = new Task(null, "Brush teeth", "Brushing teeth at 8 o'clock", TaskStatus.TODO, 2L);
+        Task task2 = new Task(null, "Morning train", "Train at 10 o'clock", TaskStatus.IN_PROGRESS, 1L);
+        Task task3 = new Task(null, "Learn Java", "Learning Java for 2 hours", TaskStatus.TODO, 1L);
+        Task task4 = new Task(null, "Meditate", "Do meditation for 10 minutes", TaskStatus.CANCELLED, 3L);
+        Task task5 = new Task(null, "Stretch", "Do stretching for 20 minutes", TaskStatus.DONE, 2L);
+        this.save(task1);
+        this.save(task2);
+        this.save(task3);
+        this.save(task4);
+        this.save(task5);
+    }
 
     public void save(Task task) {
         if (task == null) {
@@ -24,8 +52,46 @@ public class TaskRepository {
     }
 
     public Task findById(Long id) {
-        if (id == null) {
-            System.out.println("[TaskRepository] Cannot find task by id. Id is null");
+        Task task = tasks.get(id);
+        if (task == null) {
+            System.out.println("[TaskRepository] Cannot find task by id. Task with id " + id + " doesn't exist in repository");
+            return null;
         }
+        System.out.println("[TaskRepository] Task was found by id " + id + ". Returning task");
+        return task;
+    }
+
+    public List<Task> findAll() {
+        if (tasks.isEmpty()) {
+            System.out.println("[TaskRepository] Returning empty list of tasks. Repository is empty");
+            return List.of();
+        }
+        System.out.println("[TaskRepository] Returning list of tasks");
+        return new ArrayList<>(tasks.values());
+    }
+
+    public List<Task> findByStatus(TaskStatus status) {
+        if (status == null) {
+            System.out.println("[TaskRepository] Cannot return list of tasks. Status is null. Returning empty list");
+            return List.of();
+        }
+        List<Task> list = new ArrayList<>(tasks.values().stream().filter(t->t.getStatus()==status).toList());
+        if (list.isEmpty()) {
+            System.out.println("[TaskRepository] Found 0 tasks with status " + status + ". Returning empty list");
+            return list;
+        }
+        System.out.println("[TaskRepository] Returning list of tasks with status " + status);
+        return list;
+    }
+
+    public boolean deleteById(Long id) {
+        Task task = tasks.get(id);
+        if (task == null) {
+            System.out.println("[TaskRepository] Task with id " + id + " was not found. Cannot delete task");
+            return false;
+        }
+        System.out.println("[TaskRepository] Task with id " + id + " was found. Deleting task");
+        tasks.remove(id);
+        return true;
     }
 }
--- src/main/java/com/claude/lesson4/repositories/UserRepository.java
+++ src/main/java/com/claude/lesson4/repositories/UserRepository.java
@@ -1,4 +1,64 @@
 package com.claude.lesson4.repositories;
 
+import com.claude.lesson4.models.User;
+import com.claude.lesson4.models.UserRole;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class UserRepository {
+    Map<Long, User> users = new HashMap<>();
+    Long id = 0L;
+
+    public UserRepository() {
+        System.out.println("[IoC] Bean TaskRepository created");
+        initializeUsers();
+    }
+
+    private void initializeUsers() {
+        User user1 = new User(null,"Josh","josh-user@gmail.com", UserRole.USER);
+        User user2 = new User(null, "Alex", "adminalex@gmail.com", UserRole.ADMIN);
+        User user3 = new User(null, "Poatan", "poatan-butcher@mail.com", UserRole.USER);
+        User user4 = new User(null, "Marry", "marry-the-ceo@gmail.com", UserRole.MANAGER);
+        save(user1);
+        save(user2);
+        save(user3);
+        save(user4);
+    }
+
+    public void save(User user) {
+        if (user == null) {
+            System.out.println("[UserRepository] Cannot save user. User is null");
+            return;
+        }
+        if (user.getId() == null) {
+            user = new User(id++, user.getUsername(), user.getEmail(), user.getUserRole());
+        }
+        users.put(user.getId(), user);
+        System.out.println("[UserRepository] User was succesfully saved");
+    }
+
+    public User findById(Long id) {
+        User user = users.get(id);
+        if (user == null) {
+            System.out.println("[UserRepository] Cannot find user by id " + id + ". User is null");
+            return null;
+        }
+        System.out.println("[UserRepository] User was found by id. Returning user");
+        return user;
+    }
+
+    public List<User> findAll() {
+        List<User> userList = new ArrayList(users.values());
+        if (userList.isEmpty()) {
+            System.out.println("[UserRepository] Returning empty list of users. Repository is empty");
+            return userList;
+        }
+        System.out.println("[UserRepository] Returning list of users");
+        return userList;
+    }
 }
